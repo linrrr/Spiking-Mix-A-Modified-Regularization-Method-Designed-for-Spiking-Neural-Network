33904f40f48fec7b9eb14da932a782a6b3254cc0
Jaspher <52750235+linrrr@users.noreply.github.com>
2025-11-27 19:52:58 +0800

    Update neruon.py

--- neruon.py
+++ neruon.py
@@ -16,7 +16,7 @@ class LIFNode(nn.Module):
 
         v = self.v + (x - self.v) / self.tau
         oup_v = torch.clamp(v - self.v_th, 0., 1.)
-        out = self.v_th * (oup_v > self.v_th).float() + 0.5 * self.v_th * (oup_v > 0.).float()  
+        out = self.v_th * (v > self.v_th).float() + 0.5 * self.v_th * (v > 0.).float()  
         self.v = ((v - self.v_th) < 0.).float() * v
         return (out - oup_v).detach_() + oup_v
 
