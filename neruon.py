055f30d1449edc3c2ee663c335f8074a59428039
Linrrr <52750235+linrrr@users.noreply.github.com>
2025-12-07 14:21:50 +0800

    Update neruon.py

--- neruon.py
+++ neruon.py
@@ -16,8 +16,8 @@ class LIFNode(nn.Module):
 
         output_v = self.v_mul(x, self.v.data)
         oup_v = torch.clamp(output_v - self.v_th, 0., 1.)
-        out = self.encoder(output_v, self.v_th) + self.encoder(output_v, self.v_th / self.tau)
-        out = self.decoder(out)
+        out = self.enc(output_v, self.v_th) + self.encoder(output_v, self.v_th / self.tau)
+        out = self.dec(out)
         self.v = (1 - ((output_v - self.v_th) > 0.).float()) * output_v
 
         return (out - oup_v).detach_() + oup_v
@@ -26,12 +26,12 @@ class LIFNode(nn.Module):
         v = v_t + (x - v_t) / self.tau
         return v
 
-    def encoder(self, v, v_th):
+    def enc(self, v, v_th):
         return (v > v_th).float()
 
-    def decoder(self, x):
-        x = (x > self.v_th).float() * self.v_th + (x > self.v_th / self.tau).float() * (self.v_th ** 2)
-        return x
+    def dec(self, v):
+        v = (v > self.v_th).float() * self.v_th + (v > self.v_th / self.tau).float() * (self.v_th ** 2)
+        return v
 
     def reset(self):
         self.v = 0.
