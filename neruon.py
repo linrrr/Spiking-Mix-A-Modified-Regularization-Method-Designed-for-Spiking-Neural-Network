163a7556867180bb7b92897595ce2145718db232
Linrrr <52750235+linrrr@users.noreply.github.com>
2025-12-23 09:38:30 +0800

    Update neruon.py

--- neruon.py
+++ neruon.py
@@ -3,35 +3,42 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class LIFNode(nn.Module):
-    def __init__(self):
-        super(LIFNode, self).__init__()
+# v_th, tau, redefine_threshold: These required values need to be calculated.
+class LIFNeuron(nn.Module):
+    def __init__(self, v_th, tau, redefine_threshold):
+        super(LIFNeuron, self).__init__()
         self.v = 0.
-        self.v_th = 0.5
-        self.tau = 2.0
+        self.v_th = v_th
+        self.tau = tau
+        self.redefine_threshold = redefine_threshold
 
     def forward(self, x):
         if isinstance(self.v, float):
-            self.v = torch.zeros_like(x).detach().to(x.device)
+            self.v = torch.zeros_like(x).to(x.device)
 
-        output_v = self.v_mul(x, self.v.data)
-        oup_v = torch.clamp(output_v - self.v_th, 0., 1.)
-        out = self.enc(output_v, self.v_th) + self.enc(output_v, self.v_th / self.tau)
-        out = self.dec(out)
+        output_v = self.v + (x - self.v) / self.tau
+        gard_v = torch.clamp(output_v - self.v_th, 0., 1.)
+        out = (output_v > self.v_th).float() * self.redefine_weight(output_v - self.v_th)
         self.v = (1 - ((output_v - self.v_th) > 0.).float()) * output_v
 
-        return (out - oup_v).detach_() + oup_v
+        return (out - gard_v).detach_() + gard_v
 
-    def v_mul(self, x, v_t):
-        v = v_t + (x - v_t) / self.tau
-        return v
-
-    def enc(self, v, v_th):
-        return (v > v_th).float()
-
-    def dec(self, v):
-        v = (v > self.v_th * self.tau).float() * self.v_th + (v > self.v_th).float() * (self.v_th ** 2)
-        return v
+    def redefine_weight(self, x):
+        weight_value = torch.sigmoid(torch.relu(x))
+        return (weight_value > self.v_th).float() * self.redefine_threshold
 
     def reset(self):
         self.v = 0.
+
+class SharedNeuron(nn.Module):
+    def __init__(self):
+        super(SharedNeuron, self).__init__()
+        self.neuron1 = LIFNeuron(v_th, tau, redefine_threshold)
+        self.neuron2 = LIFNeuron(v_th, tau, redefine_threshold)
+
+    def forward(self, x):
+        return self.neuron1(x) + self.neuron2(x)
+
+    def reset(self):
+        self.neuron1.reset()
+        self.neuron2.reset()
