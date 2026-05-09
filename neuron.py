1fb48e4793f7142008d7e8fe32c95e38f6393d6e
Linrrr <52750235+linrrr@users.noreply.github.com>
2026-05-09 21:39:50 +0800

    Update neuron.py

--- neuron.py
+++ neuron.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class LIFNeuron(nn.Module):
-    def __init__(self, tau, v_th, sigmoid_alpha, elu_alpha, sigmoid_weight, b, w_th):
+    def __init__(self, tau, v_th, sigmoid_alpha, elu_alpha, sigmoid_weight, b):
         super(LIFNeuron, self).__init__()
         self.v = 0.
         self.tau = tau
@@ -12,7 +12,6 @@ class LIFNeuron(nn.Module):
         self.elu_alpha = elu_alpha
         self.sigmoid_weight = sigmoid_weight
         self.b = b
-        self.w_th = w_th
 
     def forward(self, x):
         if isinstance(self.v, float):
@@ -22,7 +21,7 @@ class LIFNeuron(nn.Module):
         out = (output_v > self.v_th).float()
         grad_v = F.elu(self.sigmoid_weight * torch.sigmoid(self.sigmoid_alpha * (output_v - self.v_th)) + self.b, self.elu_alpha)
         v = self.weight(out, output_v, x)
-        out = (v > self.v_th).float()
+        out = (1 - out) * v + out
         self.v = (1 - out) * output_v
 
         return (out - grad_v).detach_() + grad_v
@@ -36,8 +35,9 @@ class LIFNeuron(nn.Module):
         v = v + (x - v) / self.tau
 
         weight = self.sigmoid_weight * torch.sigmoid(torch.relu(x)) + self.b
-        weight = (weight > self.w_th).float() * self.w_th
-        return v + weight * output_v
+        weight = (weight > 1 / self.tau).float() / self.tau
+        return v * weight
 
     def reset(self):
         self.v = 0.
+
